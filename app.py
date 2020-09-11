75bebd21fd08cf3d02abdcb63012ecf1bf59373d
MikkoHimanka <mikkohimanka@protonmail.com>
2020-09-11 13:03:09 +0300

    Basic postgres interaction and login functionality

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-
-@app.route("/home/")
-def pageHome():
-    return "AAAAABBBBBdasdaa"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user == None:
+        return redirect("/")
+    else:
+        hash_value = user[0]
+        if check_password_hash(hash_value,password):
+            session["username"] = username
+
+    return redirect("/")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/newuser", methods=["POST"])
+def newuser():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password) VALUES (:username,:password)"
+    db.session.execute(sql, {"username":username,"password":hash_value})
+    db.session.commit()
+    return redirect("/")
+    
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
