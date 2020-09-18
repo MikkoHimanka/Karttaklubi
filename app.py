00d71abe2b0b4e16333732ffe776f4d3e68dd736
MikkoHimanka <mikkohimanka@protonmail.com>
2020-09-18 17:22:43 +0300

    You can now draw your maps and save them to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, redirect, request, session, url_for
+from flask import render_template, redirect, request, session, url_for, json
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -12,30 +12,61 @@ app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 
+@app.route("/maps/new_column/<int:mapcollection_id>")
+def new_column(mapcollection_id):
+    map_array_query = "SELECT * FROM mapcollections WHERE id=:mapcollection_id"
+    map_array = db.session.execute(map_array_query, {"mapcollection_id":mapcollection_id}).fetchone()
+    map_new_array = map_array[2].copy()
+
+    data = [0] * 64 * 64
+    new_map_query = "INSERT INTO maps (mapdata, editable) VALUES (:data, true)"
+    for i in range(map_array[4] - 1):
+        new_map_query += ", (:data, true)"
+    new_map_query += " RETURNING id"
+    map_id = db.session.execute(new_map_query, {"data":data}).fetchall()
+    map_id_array = list(map(lambda x: x[0], map_id))
+    for i in range(map_array[4]):
+        map_new_array[i].append(map_id_array[i])
+
+    update_query = "UPDATE mapcollections SET maps=:map_array WHERE id=:mapcollection_id"
+    db.session.execute(update_query, {"map_array": map_new_array, "mapcollection_id": mapcollection_id})
+    db.session.commit()
+
+    return redirect(url_for('maps', map_id=mapcollection_id))
 
 @app.route("/maps/new_row/<int:mapcollection_id>")
 def new_row(mapcollection_id):
-    data = [0] * 64
-    new_map_query = "INSERT INTO maps (mapdata, editable) VALUES (:data, true) RETURNING id"
-    map_id = db.session.execute(new_map_query, {"data":data}).fetchone()[0]
-    map_array_query = "SELECT maps FROM mapcollections WHERE id=:mapcollection_id"
-    map_array = db.session.execute(map_array_query, {"mapcollection_id":mapcollection_id}).fetchone()[0]
-    map_array.append(map_id)
-    update_query = "UPDATE mapcollections SET maps=:map_array WHERE id=:mapcollection_id"
-    db.session.execute(update_query, {"map_array": map_array, "mapcollection_id": mapcollection_id})
+    map_array_query = "SELECT * FROM mapcollections WHERE id=:mapcollection_id"
+    map_array = db.session.execute(map_array_query, {"mapcollection_id":mapcollection_id}).fetchone()
+    map_row_size = len(map_array[2][0])
+    map_new_rows = map_array[4] + 1
+    map_new_array = map_array[2].copy()
+
+    data = [0] * 64 * 64
+    new_map_query = "INSERT INTO maps (mapdata, editable) VALUES (:data, true)"
+    for i in range(map_row_size - 1):
+        new_map_query += ", (:data, true)"
+    new_map_query += " RETURNING id"
+    map_id = db.session.execute(new_map_query, {"data":data}).fetchall()
+    map_id_array = list(map(lambda x: x[0], map_id))
+    map_new_array.append(map_id_array)
+    print(map_new_array)
+
+    update_query = "UPDATE mapcollections SET maps=:map_array, rows=:new_rows WHERE id=:mapcollection_id"
+    db.session.execute(update_query, {"map_array": map_new_array, "mapcollection_id": mapcollection_id, "new_rows": map_new_rows})
     db.session.commit()
 
     return redirect(url_for('maps', map_id=mapcollection_id))
 
 @app.route("/newmap")
 def newmap():
-    data = [0] * 64
+    data = [0] * 64 * 64
     new_map_query = "INSERT INTO maps (mapdata, editable) VALUES (:data, true) RETURNING id"
     map_id = db.session.execute(new_map_query, {"data":data}).fetchone()[0]
     
     
-    new_mapcollections_query = "INSERT INTO mapcollections (owner, maps) VALUES (:user, ARRAY[:map_id])"
-    mapcollection_id = db.session.execute(new_mapcollections_query, {"user": session["user_id"], "map_id": map_id})
+    new_mapcollections_query = "INSERT INTO mapcollections (owner, maps, rows) VALUES (:user, ARRAY[:map_id], 1)"
+    mapcollection_id = db.session.execute(new_mapcollections_query, {"user": session["user_id"], "map_id": [map_id]})
     db.session.commit()
     
     return redirect("/")
@@ -58,8 +89,24 @@ def index():
 def maps(map_id):
     map_query = "SELECT * FROM mapcollections WHERE id=:map_id"
     map_result = db.session.execute(map_query, {"map_id": map_id}).fetchone()
+    maps = []
+    for row in map_result[2]:
+        for m in row:
+            submap_query = "SELECT id, mapdata FROM maps WHERE id=:submap_id"
+            submap_result = db.session.execute(submap_query, {"submap_id":m}).fetchone()[1]
+            maps.append(submap_result)
+    
 
-    return render_template("maps.jinja", mapcollection=map_result)
+
+    return render_template("maps.jinja", mapcollection=map_result, submaps=maps)
+
+@app.route("/editor/<int:map_id>")
+def editor(map_id):
+    map_query = "SELECT * FROM maps WHERE id=:id"
+    res = db.session.execute(map_query, {"id": map_id}).fetchone()
+    data = json.dumps(res[1])
+
+    return render_template("editor.jinja", map_data=data, map_id=map_id)
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -98,4 +145,15 @@ def newuser():
 def logout():
     del session["user_id"]
     del session["username"]
+    return redirect("/")
+
+@app.route("/save_map", methods=["POST"])
+def savemap():
+    data = json.loads(request.form["data"])
+
+    save_query = "UPDATE maps SET mapdata=:map_data WHERE id=:map_id"
+    db.session.execute(save_query, {"map_data": data, "map_id":request.form["id"]})
+    db.session.commit()
+
+
     return redirect("/")
---
+++ static/map_canvas.js
@@ -0,0 +1,83 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const imageData = context.createImageData(Math.sqrt(data.length), Math.sqrt(data.length));
+
+var hold;
+
+var mouseX;
+var mouseY;
+
+function applyColor(i, [r,g,b]) {
+    imageData.data[4*i] = r;        //R
+    imageData.data[4*i + 1] = g;    //G
+    imageData.data[4*i + 2] = b;  //B
+    imageData.data[4*i + 3] = 255;  //A
+}
+
+function createImage() {
+    for (let i = 0; i < data.length; i++) {
+        dataEntity = data[i];
+        if (dataEntity == 0) {
+            applyColor(i, [0,0,255]);
+        } else if (dataEntity <= 10) {
+            applyColor(i, [245,230,66]);
+        } else if (dataEntity <= 20) {
+            applyColor(i, [72,117,8]);
+        }
+        
+    }
+    context.putImageData(imageData, 0, 0);
+}
+createImage();
+
+///////////
+
+canvas.onmousedown = function (e) {
+    // mouseX = e.clientX - canvas.offsetLeft;
+    // mouseY = e.clientY - canvas.offsetTop;
+    draw(e);
+    hold = true;
+}
+
+canvas.onmousemove = function (e) {
+    if (hold) {
+        // mouseX = e.clientX - canvas.offsetLeft;
+        // mouseY = e.clientY - canvas.offsetTop;
+        draw(e);
+    }
+}
+
+document.onmouseup = function () {
+    hold = false;
+}
+
+
+function draw(e) {
+    container = canvas.getBoundingClientRect();
+    ratio = container.height/canvas.height;
+    
+    column = Math.floor((e.clientX - canvas.offsetLeft)/ratio);
+    row = Math.floor((e.clientY - canvas.offsetTop)/ratio);
+    
+    targetIndex = column + (row*canvas.height);
+    
+    data[targetIndex] = data[targetIndex] + 2;
+
+
+    if ((targetIndex + 1) <= data.length) {
+        data[column + 1 + (row*canvas.height)]++;
+    };
+    if ((targetIndex - 1) >= 0) {
+        data[column + (row*canvas.height) - 1]++;
+    };
+
+    if ((column + ((row+1)*canvas.height)) <= data.length) {
+        data[(column + ((row+1)*canvas.height))]++;
+    };
+    if ((column + ((row-1)*canvas.height)) >= 0) {
+        data[(column + ((row-1)*canvas.height))]++;
+    };
+
+    context.clearRect(0,0,canvas.width, canvas.height);
+    createImage();
+}
