7d3506e223e2ace8c6a0b1ceb050f95849897511
MikkoHimanka <mikkohimanka@protonmail.com>
2020-10-13 15:11:17 +0300

    New editor functions: Can now alter brush size, flow and intensity

--- static/map_canvas.js
+++ static/map_canvas.js
@@ -7,48 +7,76 @@ function execute() {
     const context = canvas.getContext('2d');
     const imageData = context.createImageData(Math.sqrt(data.length), Math.sqrt(data.length));
 
+    var showHeightMap = false;
+    var prevMouse = [99999999,99999999];
     var hold;
-
-    var mouseX;
-    var mouseY;
+    var size;
+    var flow;
+    var intensity;
 
     function applyColor(i, [r,g,b]) {
         imageData.data[4*i] = r;        //R
         imageData.data[4*i + 1] = g;    //G
-        imageData.data[4*i + 2] = b;  //B
+        imageData.data[4*i + 2] = b;    //B
         imageData.data[4*i + 3] = 255;  //A
+    }
+
+    function createHeightMap() {
+        for (let i = 0; i < data.length; i++) {
+            dataEntity = data[i];
+            applyColor(i, [dataEntity,dataEntity,dataEntity]);
+        }
+        context.putImageData(imageData, 0, 0);
     }
 
     function createImage() {
         for (let i = 0; i < data.length; i++) {
             dataEntity = data[i];
             if (dataEntity == 0) {
-                applyColor(i, [0,0,255]);
-            } else if (dataEntity <= 10) {
-                applyColor(i, [245,230,66]);
+                applyColor(i, [0, 182, 207]);
+            } else if (dataEntity <= 4) {
+                applyColor(i, [148, 218, 255]);
+            } else if (dataEntity <= 12) {
+                applyColor(i, [219, 207, 136]);
+            } else if (dataEntity <= 15) {
+                applyColor(i, [219, 204, 103]);
             } else if (dataEntity <= 20) {
-                applyColor(i, [72,117,8]);
-            }
+                applyColor(i, [176, 212, 99]);
+            } else if (dataEntity <= 25) {
+                applyColor(i, [125, 191, 54]);
+            } else if (dataEntity <= 50) {
+                applyColor(i, [55, 163, 16]);
+            } else if (dataEntity <= 7) {
+                applyColor(i, [115,115,115]);
+            } else if (dataEntity > 7) {
+                applyColor(i, [95,95,95]);
+            } 
+            
             
         }
         context.putImageData(imageData, 0, 0);
     }
     createImage();
-
-    ///////////
-
+    
     canvas.onmousedown = function (e) {
-        // mouseX = e.clientX - canvas.offsetLeft;
-        // mouseY = e.clientY - canvas.offsetTop;
-        draw(e);
+        flow = document.getElementById('flow').value/100;
+        size = document.getElementById('size').value/1;
+        intensity = document.getElementById('intensity').value/1 - 10;
+        
+        draw(e, size, intensity);
         hold = true;
     }
 
     canvas.onmousemove = function (e) {
-        if (hold) {
-            // mouseX = e.clientX - canvas.offsetLeft;
-            // mouseY = e.clientY - canvas.offsetTop;
-            draw(e);
+        container = canvas.getBoundingClientRect();
+        ratio = container.height/canvas.height;
+
+        movedEnough = (Math.abs(Math.floor((e.clientX - container.left)) - Math.floor((prevMouse[0] - container.left))) > ratio/flow) ||
+            (Math.abs(Math.floor((e.clientY - container.top)) - Math.floor((prevMouse[1] - container.top))) > ratio/flow);
+        //movedEnough = true;
+        if (hold && movedEnough) {
+            prevMouse = [e.clientX, e.clientY]
+            draw(e, size, intensity);
         }
     }
 
@@ -57,33 +85,36 @@ function execute() {
     }
 
 
-    function draw(e) {
+    function draw(e, size, intensity) {
+
         container = canvas.getBoundingClientRect();
         ratio = container.height/canvas.height;
         
         column = Math.floor((e.clientX - container.left)/ratio);
         row = Math.floor((e.clientY - container.top)/ratio);
-        
+
+        startRow = row - Math.floor(size/2-1);
+        startColumn = column - Math.floor(size/2-1)
+
         targetIndex = column + (row*canvas.height);
-        
-        data[targetIndex] = data[targetIndex] + 2;
-
-
-        if ((targetIndex + 1) <= data.length) {
-            data[column + 1 + (row*canvas.height)]++;
-        };
-        if ((targetIndex - 1) >= 0) {
-            data[column + (row*canvas.height) - 1]++;
-        };
-
-        if ((column + ((row+1)*canvas.height)) <= data.length) {
-            data[(column + ((row+1)*canvas.height))]++;
-        };
-        if ((column + ((row-1)*canvas.height)) >= 0) {
-            data[(column + ((row-1)*canvas.height))]++;
-        };
+                
+        for (var y = 0; y < (size+1); y++) {
+            for (var x = 0; x < (size+1); x++) {
+                var editIndex = (startColumn + y) + ((startRow + x)*canvas.height);
+                if (editIndex >= 0 && editIndex < data.length) {
+                    value = size/2 - Math.floor(size - Math.sqrt(Math.abs((x-size/2)**2 + (y-size/2)**2 - (size-size/4)**2)));
+                    value = value < 0 ? 0 : value;
+                    data[editIndex] += value * intensity
+                }
+            }
+        }
 
         context.clearRect(0,0,canvas.width, canvas.height);
-        createImage();
+        
+        if (showHeightMap) {
+            createHeightMap();
+        } else {
+            createImage();
+        }
     }
 }
