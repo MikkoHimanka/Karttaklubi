b2b64a41b18164b26cc38b54bc3e888675c1a118
MikkoHimanka <mikkohimanka@protonmail.com>
2020-10-16 17:40:04 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, friends INT[]);
 CREATE TABLE messages (id SERIAL PRIMARY KEY, message text, owner_id INT, time TIMESTAMP, author INT, submap BOOLEAN);
 CREATE TABLE maps (id SERIAL PRIMARY KEY, mapdata INT[], editable BOOLEAN);
-CREATE TABLE mapcollections (id SERIAL PRIMARY KEY, owner INT, maps INT[], name TEXT, rows INT, public BOOLEAN);
+CREATE TABLE mapcollections (id SERIAL PRIMARY KEY, owner INT, maps INT[], name TEXT, rows INT, public BOOLEAN, friends BOOLEAN);
+CREATE TABLE requests (id SERIAL PRIMARY KEY, sender INT, receiver INT);
